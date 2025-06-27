683d6f0d722e3bfd9eb053eaf8db0eb7214b4f77
ondrej.hlavacka <o.hlavacka@seznam.cz>
2025-06-28 00:16:19 +0200

    Lenses: composable lenses implemented.

--- src/Lenses.hs
+++ src/Lenses.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Lenses (main) where
 
 data Foo = Foo
@@ -6,13 +8,34 @@ data Foo = Foo
     }
     deriving (Show)
 
-data Lens s a = Lens {view :: s -> a, set :: s -> a -> s}
+-- (a -> a) is a function that updates a field and is promoted to a function
+-- that updates the structure (s -> s)
+type Lens s a = forall f. (Functor f) => (a -> f a) -> s -> f s
+
+newtype Identity a = Identity {runIdentity :: a}
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
+newtype Const b a = Const {runConst :: b}
+
+instance Functor (Const b) where
+    fmap _ (Const b) = Const b
+
+lens :: (s -> a) -> (s -> a -> s) -> Lens s a
+lens sa sas afa s = sas s <$> afa (sa s)
 
 barL :: Lens Foo (Int, Int)
-barL = Lens bar $ \m x -> m{bar = x}
+barL = lens bar $ \m x -> m{bar = x}
 
 _2 :: Lens (a1, a2) a2
-_2 = Lens snd $ \(a, _) x -> (a, x)
+_2 = lens snd $ \(a, _) x -> (a, x)
+
+view :: Lens s a -> s -> a
+view l s = runConst $ l Const s
+
+set :: Lens s a -> a -> s -> s
+set l a s = runIdentity $ l (const $ Identity a) s
 
 myTuple :: (Int, Int)
 myTuple = (1, 1)
@@ -22,5 +45,5 @@ myFoo = Foo myTuple 'a'
 
 main :: IO ()
 main = do
-    print $ view _2 myTuple
-    print $ set _2 myTuple 2
+    print $ view (barL . _2) myFoo
+    print $ set (barL . _2) 2 myFoo
